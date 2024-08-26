8d74df3a56e5ff96c5a47cc745e6bf2c58a6b84b
H Trayford <bowman_trayford@hotmail.com>
2024-08-26 09:46:46 -0600

    Name Tiles so we don't have to memorize the numbers

--- src/globals/Tiles.js
+++ src/globals/Tiles.js
@@ -1,1 +1,35 @@
-export const UnWalkable = [2, 4, 6]
+export const Empty = 0
+export const Stone = 1
+export const Water = 2
+export const Sand = 3
+export const RockyGround = 4
+export const Grass1 = 5
+export const Pink = 6
+export const Bridge = 7
+export const HorizontalStripe = 8
+export const Grass2 = 9
+export const Grass3 = 10
+export const Grass4 = 11
+export const Empty2 = 12
+export const DarkStone = 13
+export const Seafoam = 14
+
+export const TileNames = {
+  [Empty]: 'Empty',
+  [Stone]: 'Stone',
+  [Water]: 'Water',
+  [Sand]: 'Sand',
+  [RockyGround]: 'RockyGround',
+  [Grass1]: 'Grass1',
+  [Pink]: 'Pink',
+  [Bridge]: 'Bridge',
+  [HorizontalStripe]: 'HorizontalStripe',
+  [Grass2]: 'Grass2',
+  [Grass3]: 'Grass3',
+  [Grass4]: 'Grass4',
+  [Empty2]: 'Empty2',
+  [DarkStone]: 'DarkStone',
+  [Seafoam]: 'Seafoam',
+
+}
+export const UnWalkable = [Water, RockyGround, Pink]
