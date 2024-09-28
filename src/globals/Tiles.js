ae9420f2830e7a492781015518d7f8eac1dfe870
DaniAntSinc <cub.noble@yahoo.fr>
2024-09-28 14:26:32 -0500

    Fixing map issues, adding collision for buildings

--- src/globals/Tiles.js
+++ src/globals/Tiles.js
@@ -13,6 +13,44 @@ export const Grass4 = 11
 export const Empty2 = 12
 export const DarkStone = 13
 export const Seafoam = 14
+export const Hilltop = 10
+export const Building1 = 45
+export const Building2 = 22
+export const Building3 = 46
+export const Building4 = 21
+export const Building5 = 23
+export const Building6 = 20
+export const Building7 = 24
+export const Building8 = 42
+export const Building9 = 43
+export const Building10 = 44
+export const Building11 = 14
+export const Building12 = 16
+export const Building13 = 13
+export const Building14 = 17
+export const Building15 = 49
+export const Building16 = 26
+export const Building17 = 27
+export const Building18 = 52
+export const Building19 = 50
+export const Building20 = 53
+export const Building21 = 28
+export const Building22 = 51
+export const Building23 = 25
+export const Building24 = 29
+export const Building25 = 54
+export const Building26 = 38
+export const Building27 = 39
+export const Building28 = 40
+export const Building29 = 55
+export const Building30 = 57
+export const Building31 = 28
+export const Building32 = 56
+export const Building33 = 37
+export const Building34 = 41
+export const Building35 = 63
+export const Building36 = 36
+export const Door = 12
 
 export const TileNames = {
   [Empty]: 'Empty',
@@ -30,6 +68,44 @@ export const TileNames = {
   [Empty2]: 'Empty2',
   [DarkStone]: 'DarkStone',
   [Seafoam]: 'Seafoam',
+  [Hilltop]: 'Hilltop',
+  [Building1]: 'Building1',
+[Building2]: 'Building2',
+[Building3]: 'Building3',
+[Building4]: 'Building4',
+[Building5]: 'Building5',
+[Building6]: 'Building6',
+[Building7]: 'Building7',
+[Building8]: 'Building8',
+[Building9]: 'Building9',
+[Building10]: 'Building10',
+[Building11]: 'Building11',
+[Building12]: 'Building12',
+[Building13]: 'Building13',
+[Building14]: 'Building14',
+[Building15]: 'Building15',
+[Building16]: 'Building16',
+[Building17]: 'Building17',
+[Building18]: 'Building18',
+[Building19]: 'Building19',
+[Building20]: 'Building20',
+[Building21]: 'Building21',
+[Building22]: 'Building22',
+[Building23]: 'Building23',
+[Building24]: 'Building24',
+[Building25]: 'Building25',
+[Building26]: 'Building26',
+[Building27]: 'Building27',
+[Building28]: 'Building28',
+[Building29]: 'Building29',
+[Building30]: 'Building30',
+[Building31]: 'Building31',
+[Building32]: 'Building32',
+[Building33]: 'Building33',
+[Building34]: 'Building34',
+[Building35]: 'Building35',
+[Building36]: 'Building36',
+[Door]: 'Door',
 
 }
-export const UnWalkable = [Water, RockyGround, Pink]
+export const UnWalkable = [Water, RockyGround, Pink, Hilltop, Building1, Building2, Building3, Building4, Building5, Building6, Building7, Building8, Building9, Building10, Building11, Building12, Building13, Building14, Building15, Building16, Building17, Building18, Building19, Building20, Building21, Building22, Building23, Building24, Building25, Building26, Building27, Building28, Building29, Building30, Building31, Building32, Building33, Building34, Building35, Building36, Door]
