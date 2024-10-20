810ca7cef83d768881789222674fece745948d48
H Trayford <bowman_trayford@hotmail.com>
2024-10-20 08:00:12 -0600

    Annotate Butterfly.buildAnimations to explain how the function works/what it is doing.

--- src/entities/wildlife/Butterfly.js
+++ src/entities/wildlife/Butterfly.js
@@ -14,18 +14,25 @@ export default class Butterfly extends Insect {
   }
 
   buildAnimations () {
+    // ButterflyAnimationData is an object whose keys are the names of the animations and whose values are the animation configs
     let animationData = ButterflyAnimationData
-    const animationKeys = Object.keys(animationData)
-    animationKeys.forEach((key, index) => {
-      const config = Object.assign({}, animationData[key])
+    const animationKeys = Object.keys(animationData) // get the names of the animations as an array
+    animationKeys.forEach((key, index) => { // iterate through the array of animation names so we can get the config for each animation and build the animation
+      const config = Object.assign({}, animationData[key]) // get the animation config for the current animation name, use Object.assign to make a copy of the config
+
+      // assign additional properties to the animation config
       config.owner = this
       config.game = this.game
-      config.imageManager = this.imageManager
-      config.imageSrc = animationData[key].spritesheet
+      config.imageManager = this.imageManager // make sure the animation has access to the image manager
+      config.imageSrc = animationData[key].spritesheet // assign the image source to the animation config
+
+      // Adjust the animation duration here if you want to
+      // config.duration = config.duration + ((Math.random() * 100) - 50)
+
+      // get the image from the image manager using the image source from the animation config
       config.canvas = this.imageManager.getImageWithSrc(animationData[key].spritesheet)
-      // omg this is so complex and convoluted
-      //console.log("my img url is: "+animationData[key].spritesheet);
-      //console.log("new butterfly with a canvas",config.canvas);
+
+      // create a new Animation object using the config and assign it to the animations object using the same key as exists in the ButterflyAnimationData object
       this.animations[key] = new Animation(config)
     })
   }
