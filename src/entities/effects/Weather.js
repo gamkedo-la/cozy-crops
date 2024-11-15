f7221aa5db2fb7b8fecb5261605dfac8b4008603
Christer Kaitila <breakdance@mcfunkypants.com>
2024-11-15 09:43:26 -0800

    wind and rain sounds fade in and out

--- src/entities/effects/Weather.js
+++ src/entities/effects/Weather.js
@@ -30,11 +30,18 @@ export default class Weather {
     this.snowImg = new Image();
     this.snowImg.src = "../../img/snow.png"
     this.snowImg.onload = function() { this.loaded=true }
+    this.windSound = new Audio("../../sounds/wind.ogg")
+    this.windSound.volume = 0
+    this.windSound.loop = true
+    this.rainSound = new Audio("../../sounds/rain.ogg")
+    this.rainSound.volume = 0
+    this.rainSound.loop = true
   }
 
   startRaining() {
     console.log("weather system: rain begins")
     this.rainFadeDelta = this.FADESPEED
+    this.rainSound.play()
   }
 
   stopRaining() {
@@ -75,6 +82,7 @@ export default class Weather {
   getWindy() {
     console.log("weather system: wind picks up")
     this.windFadeDelta = this.FADESPEED
+    this.windSound.play()
   }
 
   stopWind() {
@@ -84,29 +92,40 @@ export default class Weather {
 
   update (deltaTime) {
     this.age += deltaTime
+    /*
+    console.log("weather update:"+
+        " age:"+this.age.toFixed(0)+
+        " fog:"+this.fogStrength.toFixed(1)+ 
+        " cool:"+this.coolBlueStrength.toFixed(1)+ 
+        " rain:"+this.rainStrength.toFixed(1)+ 
+        " snow:"+this.snowStrength.toFixed(1)+ 
+        " wind:"+this.windStrength.toFixed(1))
+    */
     if (CheatKeys) {
         const justDownKeys = this.scene.inputManager.getJustDownKeys()
-        if (justDownKeys.includes(F)) { this.fogStrength<=0?this.getFoggy():this.clearFog() }
-        if (justDownKeys.includes(C)) { this.coolBlueStrength<=0?this.getCool():this.getWarm() }
-        if (justDownKeys.includes(R)) { this.rainStrength<=0?this.startRaining():this.stopRaining() }
-        if (justDownKeys.includes(V)) { this.snowStrength<=0?this.startSnowing():this.stopSnowing() }
-        if (justDownKeys.includes(G)) { this.windStrength<=0?this.getWindy():this.stopWind() }
+        if (justDownKeys.includes(F)) { !this.fogStrength?this.getFoggy():this.clearFog() }
+        if (justDownKeys.includes(C)) { !this.coolBlueStrength?this.getCool():this.getWarm() }
+        if (justDownKeys.includes(R)) { !this.rainStrength?this.startRaining():this.stopRaining() }
+        if (justDownKeys.includes(V)) { !this.snowStrength?this.startSnowing():this.stopSnowing() }
+        if (justDownKeys.includes(G)) { !this.windStrength?this.getWindy():this.stopWind() }
     }
     this.coolBlueStrength += (deltaTime/1000) * this.coolBlueFadeDelta
     this.fogStrength += (deltaTime/1000) * this.fogFadeDelta
     this.rainStrength += (deltaTime/1000) * this.rainFadeDelta
     this.snowStrength += (deltaTime/1000) * this.snowFadeDelta
-    this.windStrength += (deltaTime/1000) * this.windStrengthFadeDelta
-    if (this.coolBlueStrength>=1) { this.coolBlueStrength=1; this.coolBlueFadeDelta = 0 }
-    if (this.fogStrength>=1) { this.fogStrength=1; this.fogFadeDelta = 0 }
-    if (this.rainStrength>=1) { this.rainStrength=1; this.rainFadeDelta = 0 }
-    if (this.snowStrength>=1) { this.snowStrength=1; this.snowFadeDelta = 0 }
-    if (this.windStrength>=1) { this.windStrength=1; this.windStrengthFadeDelta = 0 }
-    if (this.coolBlueStrength<=0) { this.coolBlueStrength=0; this.coolBlueFadeDelta = 0 }
-    if (this.fogStrength<=0) { this.fogStrength=0; this.fogFadeDelta = 0 }
-    if (this.rainStrength<=0) { this.rainStrength=0; this.rainFadeDelta = 0 }
-    if (this.snowStrength<=0) { this.snowStrength=0; this.snowFadeDelta = 0 }
-    if (this.windStrength<=0) { this.windStrength=0; this.windStrengthFadeDelta = 0 }
+    this.windStrength += (deltaTime/1000) * this.windFadeDelta
+    if (this.coolBlueStrength>1) this.coolBlueStrength=1
+    if (this.fogStrength>1) this.fogStrength=1
+    if (this.rainStrength>1) this.rainStrength=1
+    if (this.snowStrength>1) this.snowStrength=1
+    if (this.windStrength>1) this.windStrength=1
+    if (this.coolBlueStrength<0) this.coolBlueStrength=0
+    if (this.fogStrength<0) this.fogStrength=0
+    if (this.rainStrength<0) this.rainStrength=0
+    if (this.snowStrength<0) this.snowStrength=0
+    if (this.windStrength<0) this.windStrength=0
+    this.windSound.volume = this.windStrength>0?this.windStrength/3:0
+    this.rainSound.volume = this.rainStrength>0?this.rainStrength/3:0
   }
 
   draw (camera) {
