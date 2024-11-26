1e1370c9d28117ab1ade8fde6c9bd93ce3d8bef6
Christer Kaitila <breakdance@mcfunkypants.com>
2024-11-26 10:17:13 -0800

    particles are rendered but camera offset is calculated incorrectly

--- src/entities/effects/Particles.js
+++ src/entities/effects/Particles.js
@@ -4,29 +4,26 @@ import { ParticleSprite } from "../../globals/Images.js"
 
 export default class Particles {
 
-  constructor (config) {
-    Object.assign(this, config)
-    this.age = 0
-    this.img = this.imageManager.getImageWithSrc(ParticleSprite)
-    this.pool = []
-  }
+    constructor (config) {
+        Object.assign(this, config)
+        this.age = 0
+        this.img = this.imageManager.getImageWithSrc(ParticleSprite)
+        this.sprW = 32
+        this.sprH = 32
+        this.drag = 0.94
+        this.gravity = -0.01
+        this.pool = []
+    }
 
-  add (x, y, life, size, rotationSpeed, forcedAngle, velX, velY, myAlpha) {
+    add (x, y, life=1000, size=32, rotSpd=0, angle=0, velX=0, velY=0, alpha=1, drag=0.94) {
         let p = null
-        if (life === undefined) life = 1000
-        if (size === undefined) size = 32
-        if (velX === undefined) velX = 0
-        if (velY === undefined) velY = 0
-        if (myAlpha === undefined) myAlpha = 1
-        if (rotationSpeed === undefined) rotationSpeed = Math.random() * 3 - 2
-        if (forcedAngle === undefined) forcedAngle = 0
         for (let pnum = 0; pnum < this.pool.length; pnum++) {
             p = this.pool[pnum]
             if (p && p.inactive) { break; } // found one we can reuse
         }
         if (!p || !p.inactive) { // we need a new one
             var newParticle = { inactive: true }
-            particle.push(newParticle)
+            this.pool.push(newParticle)
             p = newParticle
         }
         if (p && p.inactive) { // reuse an old one
@@ -38,59 +35,108 @@ export default class Particles {
             p.life = life
             p.birth = this.age
             p.death = p.birth + life
-            p.angle = forcedAngle
-            p.alpha = myAlpha
-            p.maxalpha = myAlpha
-            p.rotSpd = rotationSpeed
+            p.angle = angle
+            p.alpha = alpha
+            p.maxalpha = alpha
+            p.rotSpd = rotSpd
             p.velX = velX
             p.velY = velY
+            p.drag = drag
         }
-  }
+    }
 
-  update (deltaTime) {
-    this.age += deltaTime // in ms (1000 per second)
-    //console.log("particles update:"+" age:"+this.age.toFixed(0))
-    this.pool.forEach(
-        function (p) {
+    update (deltaTime) {
+        this.age += deltaTime // in ms (1000 per second)
+
+        // for debug purposes only, spawn particles in predefined hardcoded locations
+        // (eventually there will be an emitter entity class with x,y,loop etc)
+        // 1200,1200 is near the player start pos
+        this.splash(1200,1200)
+
+        //console.log("particles update:"+" age:"+this.age.toFixed(0))
+        for (let p of this.pool) {
             if (!p.inactive) {
                 p.age = this.age - p.birth
-                var lifePercent = (p.age / p.life)
+                let lifePercent = (p.age / p.life)
                 if (lifePercent > 1) lifePercent = 1
                 if (lifePercent < 0) lifePercent = 0
                 p.x += p.velX // move
                 p.y += p.velY
-                p.velX *= 0.94 // slow down
-                p.velY *= 0.94
+                p.velX *= p.drag // slow down
+                p.velY *= p.drag
                 p.scale = p.size * lifePercent // grow
                 p.alpha = (1 - lifePercent) * p.maxalpha // fade
                 p.angle = Math.PI * 2 * lifePercent * p.rotSpd
                 if (this.age >= p.death) p.inactive = true
             }
-        })
+        }
     }
-
-    draw (offsetx=0,offsety=0) {
+    
+    draw () {
         //console.log("particles draw:"+" age:"+this.age.toFixed(0))
-        this.game.ctx.save()
-        this.game.ctx.translate(offsetx,offsety)
-        this.pool.forEach(
-            function (p) {
-                if (!p.inactive) {
-					this.game.ctx.save()
-					this.game.ctx.translate(p.x, p.y)
-					if (p.angle !== undefined) this.game.ctx.rotate(p.angle)
-					if (p.alpha !== undefined) this.game.ctx.globalAlpha = p.alpha
-					this.game.ctx.drawImage(p.sprite, -p.sprite.width / 2, -p.sprite.height / 2)
-					this.game.ctx.restore()
-                }
+        //let ctx = this.imageManager.internalCtx; // nothing visible... perhaps already used this frame and about to be cleared
+        let ctx = this.game.ctx 
+        ctx.save()
+
+        // FIXME: these all seem to scroll at the wrong speed
+        //let offset = {x:0,y:0}
+        //let offset = this.imageManager.camera.getTopLeft()
+        //let offset = {x:this.scene.camera.x,y:this.scene.camera.y}
+        let offset = this.scene.camera.getTopLeft()
+
+        ctx.translate(-offset.x,-offset.y) 
+
+        for (let p of this.pool) {
+            if (!p.inactive) {
+                ctx.save()
+                ctx.translate(p.x, p.y)
+                //ctx.translate(p.x-offset.x, p.y-offset.y)
+                if (p.angle !== undefined) ctx.rotate(p.angle)
+                if (p.alpha !== undefined) ctx.globalAlpha = p.alpha
+                ctx.drawImage(p.sprite, -p.sprite.width / 2, -p.sprite.height / 2)
+                ctx.restore()
             }
-        )
-        this.game.ctx.restore()
+        }
+
+        ctx.globalAlpha = 1
+        ctx.restore()
     }
 
     // immediately clears all particles
     clear () { this.pool = [] }
-}
+
+    // maybe spawn a fire particle
+    fire(x,y) {
+        let num = randomInt(0,1) // sometimes adds none
+        for (let i = 0; i < num; i++) {
+            let life = randomInt(333,777)
+            let size = randomInt(1,4)
+            let rotspd = Math.random()*0.3-0.15
+            let ang = 0
+            let velx = Math.random()*3-1.5
+            let vely = Math.random()*-1.5
+            let alpha = 0.5
+            this.add(x,y,life,size,rotspd,ang,velx,vely,alpha)
+        }
+    }
+
+    splash(x,y) {
+        let num = randomInt(4,8) 
+        for (let i = 0; i < num; i++) {
+            let life = randomInt(333,777)
+            let size = randomInt(1,4)
+            let rotspd = Math.random()*0.3-0.15
+            let ang = 0
+            let velx = Math.random()*3-1.5
+            let vely = Math.random()*-1.5
+            let alpha = 0.25 // start out quite faint
+            let px = Math.round(x+Math.random()*64-32)
+            let py = Math.round(y+Math.random()*4-2)
+            this.add(px,py,life,size,rotspd,ang,velx,vely,alpha)
+        }
+    }
+
+} // end of Particles class
 
 // helper function (inclusive: eg 1,10 may include 1 or 10)
 function randomInt(min,max) { return Math.floor(Math.random()*(max-min+1))+min }
