fceedd5ca07a90c861c37980880e7064a56b1348
H Trayford <bowman_trayford@hotmail.com>
2024-12-08 12:25:24 -0700

    All Crop Animation Data

--- src/globals/CropAnimations.js
+++ src/globals/CropAnimations.js
@@ -187,7 +187,110 @@ export const Corn = {
   Inventory: {
     name: 'CornInventory',
     spritesheet: TallCrops,
-    frames: [32],
+    frames: [31],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Eggplant = {
+  Seedling: {
+    name: 'EggplantSeedling',
+    spritesheet: TallCrops,
+    frames: [17],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'EggplantYoungSprout',
+    spritesheet: TallCrops,
+    frames: [18],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'EggplantSprout',
+    spritesheet: TallCrops,
+    frames: [19],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'EggplantYoungPlantIdle',
+    spritesheet: TallCrops,
+    frames: [20],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'EggplantYoungPlantWind',
+    spritesheet: TallCrops,
+    frames: [20, 21, 22],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'EggplantMaturePlantIdle',
+    spritesheet: TallCrops,
+    frames: [23],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'EggplantMaturePlantWind',
+    spritesheet: TallCrops,
+    frames: [23, 24, 25],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'EggplantDead',
+    spritesheet: TallCrops,
+    frames: [26],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Harvested: {
+    name: 'EggplantHarvested',
+    spritesheet: TallCrops,
+    frames: [27],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'EggplantInventory',
+    spritesheet: TallCrops,
+    frames: [16],
     frameWidth: 16,
     frameHeight: 32,
     padding: 1,
@@ -382,11 +485,690 @@ export const Onion = {
   }
 }
 
+export const Pepper = {
+  Seedling: {
+    name: 'PepperSeedling',
+    spritesheet: TallCrops,
+    frames: [2],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'PepperYoungSprout',
+    spritesheet: TallCrops,
+    frames: [3],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'PepperSprout',
+    spritesheet: TallCrops,
+    frames: [4],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'PepperYoungPlantIdle',
+    spritesheet: TallCrops,
+    frames: [5],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'PepperYoungPlantWind',
+    spritesheet: TallCrops,
+    frames: [5, 6, 7],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'PepperMaturePlantIdle',
+    spritesheet: TallCrops,
+    frames: [8],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'PepperMaturePlantWind',
+    spritesheet: TallCrops,
+    frames: [8, 9, 10],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'PepperDead',
+    spritesheet: TallCrops,
+    frames: [11],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Harvested: {
+    name: 'PepperHarvested',
+    spritesheet: TallCrops,
+    frames: [12],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'PepperInventory',
+    spritesheet: TallCrops,
+    frames: [1],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Potato = {
+  Seedling: {
+    name: 'PotatoSeedling',
+    spritesheet: ShortCrops,
+    frames: [74],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'PotatoYoungSprout',
+    spritesheet: ShortCrops,
+    frames: [75],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'PotatoSprout',
+    spritesheet: ShortCrops,
+    frames: [76],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'PotatoYoungPlantIdle',
+    spritesheet: ShortCrops,
+    frames: [77],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'PotatoYoungPlantWind',
+    spritesheet: ShortCrops,
+    frames: [77, 78, 79],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'PotatoMaturePlantIdle',
+    spritesheet: ShortCrops,
+    frames: [80],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'PotatoMaturePlantWind',
+    spritesheet: ShortCrops,
+    frames: [80, 81, 82],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'PotatoDead',
+    spritesheet: ShortCrops,
+    frames: [83],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'PotatoInventory',
+    spritesheet: ShortCrops,
+    frames: [73],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Pumpkin = {
+  Seedling: {
+    name: 'PumpkinSeedling',
+    spritesheet: ShortCrops,
+    frames: [62],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'PumpkinYoungSprout',
+    spritesheet: ShortCrops,
+    frames: [63],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'PumpkinSprout',
+    spritesheet: ShortCrops,
+    frames: [64],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'PumpkinYoungPlantIdle',
+    spritesheet: ShortCrops,
+    frames: [65],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'PumpkinYoungPlantWind',
+    spritesheet: ShortCrops,
+    frames: [65, 66, 67],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'PumpkinMaturePlantIdle',
+    spritesheet: ShortCrops,
+    frames: [68],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'PumpkinMaturePlantWind',
+    spritesheet: ShortCrops,
+    frames: [68, 69, 70],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'PumpkinDead',
+    spritesheet: ShortCrops,
+    frames: [71],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'PumpkinInventory',
+    spritesheet: ShortCrops,
+    frames: [61],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Radish = {
+  Seedling: {
+    name: 'RadishSeedling',
+    spritesheet: ShortCrops,
+    frames: [86],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'RadishYoungSprout',
+    spritesheet: ShortCrops,
+    frames: [87],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'RadishSprout',
+    spritesheet: ShortCrops,
+    frames: [88],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'RadishYoungPlantIdle',
+    spritesheet: ShortCrops,
+    frames: [89],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'RadishYoungPlantWind',
+    spritesheet: ShortCrops,
+    frames: [89, 90, 91],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'RadishMaturePlantIdle',
+    spritesheet: ShortCrops,
+    frames: [92],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'RadishMaturePlantWind',
+    spritesheet: ShortCrops,
+    frames: [92, 93, 94],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'RadishDead',
+    spritesheet: ShortCrops,
+    frames: [95],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'RadishInventory',
+    spritesheet: ShortCrops,
+    frames: [85],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Strawberry = {
+  Seedling: {
+    name: 'StrawberrySeedling',
+    spritesheet: TallCrops,
+    frames: [47],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'StrawberryYoungSprout',
+    spritesheet: TallCrops,
+    frames: [48],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'StrawberrySprout',
+    spritesheet: TallCrops,
+    frames: [49],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'StrawberryYoungPlantIdle',
+    spritesheet: TallCrops,
+    frames: [50],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'StrawberryYoungPlantWind',
+    spritesheet: TallCrops,
+    frames: [50, 51, 52],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'StrawberryMaturePlantIdle',
+    spritesheet: TallCrops,
+    frames: [53],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'StrawberryMaturePlantWind',
+    spritesheet: TallCrops,
+    frames: [53, 54, 55],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'StrawberryDead',
+    spritesheet: TallCrops,
+    frames: [56],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Harvested: {
+    name: 'StrawberryHarvested',
+    spritesheet: TallCrops,
+    frames: [57],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'StrawberryInventory',
+    spritesheet: TallCrops,
+    frames: [46],
+    frameWidth: 16,
+    frameHeight: 32,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Tomato = {
+  Seedling: {
+    name: 'TomatoSeedling',
+    spritesheet: ShortCrops,
+    frames: [38],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'TomatoYoungSprout',
+    spritesheet: ShortCrops,
+    frames: [39],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'TomatoSprout',
+    spritesheet: ShortCrops,
+    frames: [40],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'TomatoYoungPlantIdle',
+    spritesheet: ShortCrops,
+    frames: [41],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'TomatoYoungPlantWind',
+    spritesheet: ShortCrops,
+    frames: [41, 42, 43],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'TomatoMaturePlantIdle',
+    spritesheet: ShortCrops,
+    frames: [44],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'TomatoMaturePlantWind',
+    spritesheet: ShortCrops,
+    frames: [44, 45, 46],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'TomatoDead',
+    spritesheet: ShortCrops,
+    frames: [47],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'TomatoInventory',
+    spritesheet: ShortCrops,
+    frames: [37],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
+export const Watermelon = {
+  Seedling: {
+    name: 'WatermelonSeedling',
+    spritesheet: ShortCrops,
+    frames: [50],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungSprout: {
+    name: 'WatermelonYoungSprout',
+    spritesheet: ShortCrops,
+    frames: [51],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Sprout: {
+    name: 'WatermelonSprout',
+    spritesheet: ShortCrops,
+    frames: [52],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantIdle: {
+    name: 'WatermelonYoungPlantIdle',
+    spritesheet: ShortCrops,
+    frames: [53],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  YoungPlantWind: {
+    name: 'WatermelonYoungPlantWind',
+    spritesheet: ShortCrops,
+    frames: [53, 54, 55],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  MaturePlantIdle: {
+    name: 'WatermelonMaturePlantIdle',
+    spritesheet: ShortCrops,
+    frames: [56],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  MaturePlantWind: {
+    name: 'WatermelonMaturePlantWind',
+    spritesheet: ShortCrops,
+    frames: [56, 57, 58],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: false
+  },
+  Dead: {
+    name: 'WatermelonDead',
+    spritesheet: ShortCrops,
+    frames: [59],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  },
+  Inventory: {
+    name: 'WatermelonInventory',
+    spritesheet: ShortCrops,
+    frames: [49],
+    frameWidth: 16,
+    frameHeight: 16,
+    padding: 1,
+    duration: 500,
+    loop: true
+  }
+}
+
 const CropAnimations = {
   Carrot,
   Corn,
+  Eggplant,
   Lettuce,
-  Onion
+  Onion,
+  Pepper,
+  Potato,
+  Pumpkin,
+  Radish,
+  Strawberry,
+  Tomato,
+  Watermelon
 }
 
 export default CropAnimations
