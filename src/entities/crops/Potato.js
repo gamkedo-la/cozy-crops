bcdc80e113ebb1193e7c248d96e9c12dcdee964f
McMahem <87210679+McMahem@users.noreply.github.com>
2024-12-15 15:53:05 -0500

    Potato, Pumpkin, Strawberry - built off of Lettuce.js
    Changed "Lettuce" to respective crop word.

--- src/entities/crops/Potato.js
+++ src/entities/crops/Potato.js
@@ -1,1 +1,37 @@
-// Needs to be implemented. Look at Carrot.js or Lettuce.js for reference
+import Crop from './Crop.js'
+import CropAnimations from '../../globals/CropAnimations.js'
+import CropData from '../../globals/CropData.js'
+import Animation from '../../components/Animation.js'
+import EntityTypes from '../../globals/EntityTypes.js'
+
+export default class Potato extends Crop {
+  constructor (config) {
+    config.type = EntityTypes.Potato
+    config = {...config, ...CropData[EntityTypes.Potato]}
+    super(config)
+  }
+
+  buildAnimations () {
+    const animationKeys = Object.keys(CropAnimations.Potato)
+    animationKeys.forEach((key, index) => {
+      const config = Object.assign({}, CropAnimations.Potato[key])
+      config.owner = this
+      config.game = this.game
+      config.imageManager = this.imageManager
+      config.imageSrc = CropAnimations.Potato[key].spritesheet
+      config.canvas = this.imageManager.getImageWithSrc(CropAnimations.Potato[key].spritesheet)
+      this.animations[key] = new Animation(config)
+    })
+  }
+
+  getAnimation (type = 'Idle') {
+    if (this.currentGrowthStage < 0) {
+      return this.animations['Dead']
+    } else if (this.growthStages[this.currentGrowthStage] === 'YoungPlant' || this.growthStages[this.currentGrowthStage] === 'MaturePlant') {
+      const key = this.growthStages[this.currentGrowthStage] + type
+      return this.animations[key]
+    } else {
+      return this.animations[this.growthStages[this.currentGrowthStage]]
+    }
+  }
+}
--- src/entities/crops/Pumpkin.js
+++ src/entities/crops/Pumpkin.js
@@ -1,1 +1,37 @@
-// Needs to be implemented. Look at Carrot.js or Lettuce.js for reference
+import Crop from './Crop.js'
+import CropAnimations from '../../globals/CropAnimations.js'
+import CropData from '../../globals/CropData.js'
+import Animation from '../../components/Animation.js'
+import EntityTypes from '../../globals/EntityTypes.js'
+
+export default class Pumpkin extends Crop {
+  constructor (config) {
+    config.type = EntityTypes.Pumpkin
+    config = {...config, ...CropData[EntityTypes.Pumpkin]}
+    super(config)
+  }
+
+  buildAnimations () {
+    const animationKeys = Object.keys(CropAnimations.Pumpkin)
+    animationKeys.forEach((key, index) => {
+      const config = Object.assign({}, CropAnimations.Pumpkin[key])
+      config.owner = this
+      config.game = this.game
+      config.imageManager = this.imageManager
+      config.imageSrc = CropAnimations.Pumpkin[key].spritesheet
+      config.canvas = this.imageManager.getImageWithSrc(CropAnimations.Pumpkin[key].spritesheet)
+      this.animations[key] = new Animation(config)
+    })
+  }
+
+  getAnimation (type = 'Idle') {
+    if (this.currentGrowthStage < 0) {
+      return this.animations['Dead']
+    } else if (this.growthStages[this.currentGrowthStage] === 'YoungPlant' || this.growthStages[this.currentGrowthStage] === 'MaturePlant') {
+      const key = this.growthStages[this.currentGrowthStage] + type
+      return this.animations[key]
+    } else {
+      return this.animations[this.growthStages[this.currentGrowthStage]]
+    }
+  }
+}
