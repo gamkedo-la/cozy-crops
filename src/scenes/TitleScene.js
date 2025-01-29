b7f387c4b35b70df6e6a2b0c3285c618bd9c1dc1
Christer Kaitila <breakdance@mcfunkypants.com>
2025-01-29 14:23:29 -0800

    bunny added to title screen

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,6 +1,6 @@
 import Scene from './Scene.js'
 import Scenes from '../globals/Scenes.js'
-import { TitleBackground, TitleBackground2, TitleBackground3, TitleBackground4 } from '../globals/Images.js'
+import { TitleBackground, TitleBackground2, TitleBackground3, TitleBackground4, WildlifeSprites } from '../globals/Images.js'
 import Menu from '../uiElements/Menu.js'
 import Constants from '../globals/Constants.js'
 import UIAttributes from '../globals/UIAttributes.js'
@@ -23,6 +23,24 @@ export default class TitleScene extends Scene {
     this.backgroundImage3 = this.imageManager.getImageWithSrc(TitleBackground3)
     this.backgroundImage4 = this.imageManager.getImageWithSrc(TitleBackground4)
 
+    // fixme: titlescene is not a proper game scene - no way to render without much hassle
+    /*
+    this.titleBunny = new Bunny({
+        game: this.game,
+        scene: this,
+        imageManager: this.imageManager,
+        particles: this.particles,
+        x: 100,
+        y: 100
+      })
+    this.titleBunny.init()
+    */
+
+    // manual solution to avoid state/scope/scene issues =(
+    this.titleBunny = new Image();
+    this.titleBunny.src = "img/Wildlife.png"; // fixme: ENV
+    this.titleBunny.onload = function() { this.loaded=true; }
+
     // setup resources
     this.menu = new Menu({
       x: (this.game.canvas.width / (2 * Constants.CanvasScale)) - 65,
@@ -43,17 +61,15 @@ export default class TitleScene extends Scene {
 
   update (deltaTime) {
     super.update(deltaTime) // Call the update method of the parent class
-
     manageInput(this)
     this.menu.update(deltaTime)
   }
 
   draw () {
     super.draw() // Call the draw method of the parent class
-
     drawBackground(this)
+    //this.titleBunny.draw() // sprites don't work on the titlescreen
     this.menu.draw()
-
     this.imageManager.drawOverlay()
   }
 
@@ -85,20 +101,34 @@ function drawBitmapCenteredWithRotation(ctx, img, atX, atY, withAng) {
 }
 
 function drawBackground (title) {
+  
   // sky and clouds
   title.game.ctx.drawImage(title.backgroundImage, 0, 0, title.game.canvas.width, title.game.canvas.height)
+  
   // rotating sunshine effect
   drawBitmapCenteredWithRotation(title.game.ctx, title.backgroundImage2,
     Math.round(title.game.canvas.width / 2),
     Math.round(title.game.canvas.height / 2), 
     performance.now() / 8000
   )
+  
   // horizon and rows of crops
   title.game.ctx.drawImage(title.backgroundImage3, 0, 0, title.game.canvas.width, title.game.canvas.height)
+  
   // centered logo
   title.game.ctx.drawImage(title.backgroundImage4,
     Math.round(title.game.canvas.width / 2 - title.backgroundImage4.width / 2),
     Math.round(-250 + title.game.canvas.height / 2 - title.backgroundImage4.height / 2))
+  
+  // and finally, just for fun, a bunny nibbling on our crops 
+  // blah drawing is so hard in this engine, this is try #5
+  // title.game.ctx.drawImage(this.imageManager.getImageWithSrc(WildlifeSprites),0,0); // hmm try getImageWithSrc? fixme: camera not set up yet
+  if (title.titleBunny.loaded) {
+    let sx=0, sy=16, sw=16, sh=16, dx=810, dy=660, dw=32, dh=32
+    let ofs = Math.floor(performance.now()/777 % 6) * 16
+    title.game.ctx.drawImage(title.titleBunny,sx+ofs,sy,sw,sh,dx,dy,dw,dh)
+  }
+
 }
 
 function manageInput (scene) {
